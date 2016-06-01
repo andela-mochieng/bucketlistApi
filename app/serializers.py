e0f26e15ae9081912ebfbedc6afd154135fd7f92
Margaret Ochieng <margaret.ochieng@andela.com>
2016-06-01 12:30:49 +0300

    Create format used by marshall to map the models

---
+++ app/serializers.py
@@ -0,0 +1,25 @@
+"""Module defines the format used by marshall to map the models."""
+from flask_restful import fields
+
+bucketlistitem_serializer = {
+    'item_id': fields.Integer,
+    'item_name': fields.String,
+    'done': fields.Boolean,
+    'date_created': fields.DateTime,
+    'date_modified': fields.DateTime
+}
+
+bucketlist_serializer = {
+    'id': fields.Integer,
+    'list_name': fields.String,
+    'bucketlist_items': fields.Nested(bucketlistitem_serializer),
+    'created_by': fields.Integer,
+    'date_created': fields.DateTime,
+    'date_modified': fields.DateTime
+}
+
+user_serializer = {
+    'id': fields.Integer,
+    'username': fields.String,
+    'bucketlists': fields.Nested(bucketlist_serializer)
+}
